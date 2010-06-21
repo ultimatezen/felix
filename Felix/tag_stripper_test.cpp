c3e4a64a2a029d2088769febf6e0b97665c45382
Ryan Ginstrom <software@ginstrom.com>
2010-06-21 21:45:04 +0900

    Added tests for new tag_stripper functionality

--- Felix/tag_stripper_test.cpp
+++ Felix/tag_stripper_test.cpp
@@ -44,18 +44,50 @@ BOOST_AUTO_TEST_SUITE( test_tag_stripper )
 			L"with</FONT> <U><FONT color=#ff0000>lots of "
 			L"<STRONG>formatting</STRONG></FONT></U>!</TD>" ;
 		wstring stripped = strip_tags(rich) ;
-		BOOST_CHECK ( stripped == L"Here is some test text with lots of formatting!" ) ; 
+		BOOST_CHECK_EQUAL( stripped, wstring(L"Here is some test text with lots of formatting!") ) ; 
 	}
+	BOOST_AUTO_TEST_CASE( test_entity)
+	{
+		wstring rich = L"<STRONG>&amp;</STRONG>" ;
+		wstring stripped = strip_tags(rich) ;
+		BOOST_CHECK_EQUAL( stripped, wstring(L"&") ) ; 
+	}
+
 	BOOST_AUTO_TEST_CASE( test_variations)
 	{
 		wstring rich = L"<LI><STRONG><U><EM><FONT color=#0080ff>Variations on the theme...</FONT></EM></U></STRONG>" ;
 		wstring stripped = strip_tags(rich) ;
-		BOOST_CHECK ( stripped == L"Variations on the theme..." ) ; 
+		BOOST_CHECK_EQUAL( stripped, wstring(L"Variations on the theme...") ) ; 
 	}
 	BOOST_AUTO_TEST_CASE( vertical_tab )
 	{
 		wstring rich = L"<b>Here</b> is a &#11; (vertical tab)" ;
 		wstring stripped = strip_tags( rich ) ;
 		BOOST_CHECK_EQUAL( stripped, wstring(L"Here is a ") + wchar_t(11) + wstring(L" (vertical tab)") ) ;
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( test_strip_tags_only )
+	// strip_tags
+	BOOST_AUTO_TEST_CASE( test_foobar )
+	{
+		wstring rich = L"<b>foo</b><font size=3><i>bar</i></font> " ;
+		wstring stripped = strip_tags_only( rich ) ;
+		BOOST_CHECK_EQUAL( stripped, L"foobar " ) ;
+	}
+	BOOST_AUTO_TEST_CASE( test_complex)
+	{
+		wstring rich = L"<TD>Here <FONT face=\"Monotype Corsiva\"><EM>is <U>some </U>test</EM> text "
+			L"with</FONT> <U><FONT color=#ff0000>lots of "
+			L"<STRONG>formatting</STRONG></FONT></U>!</TD>" ;
+		wstring stripped = strip_tags_only(rich) ;
+		BOOST_CHECK_EQUAL( stripped, wstring(L"Here is some test text with lots of formatting!") ) ; 
+	}
+	BOOST_AUTO_TEST_CASE( test_entity)
+	{
+		wstring rich = L"<STRONG>&amp;</STRONG>" ;
+		wstring stripped = strip_tags_only(rich) ;
+		BOOST_CHECK_EQUAL( stripped, wstring(L"&amp;") ) ; 
 	}
 BOOST_AUTO_TEST_SUITE_END()
