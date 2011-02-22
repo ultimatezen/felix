65a502f4d6af7c5c06fde886877fd5ba0262db27
ginstrom <ginstrom@localhost>
2011-02-23 00:22:28 +0900

    QC Gloss check

--- common/qcrules/gloss_check.h
+++ common/qcrules/gloss_check.h
@@ -1,6 +1,44 @@
 #pragma once
 
+#include "check_rule.h"
+
+#include <vector>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+
+#include <boost/foreach.hpp>
+using namespace boost::foreach ;
+
 namespace qc
 {
+	class GlossCheckRule : public CheckRule
+	{
+		typedef std::pair<std::wstring, std::wstring> gloss_pair ;
 
+		std::vector<gloss_pair> m_gloss_matches ;
+
+	public:
+		void clear_gloss_matches()
+		{
+			m_gloss_matches.clear() ;
+		}
+		void add_gloss_match(const std::wstring source, const std::wstring target)
+		{
+			m_gloss_matches.push_back(gloss_pair(source, target)) ;
+		}
+		bool check(const std::wstring source, const std::wstring target)
+		{
+			BOOST_FOREACH(gloss_pair gloss, m_gloss_matches)
+			{
+				if (! boost::contains(target, gloss.second))
+				{
+					std::wstring message = (boost::wformat(L"Gloss Check: Word '%s' in source but '%s' not in target") % gloss.first % gloss.second).str() ;
+					m_messages.push_back(message) ;
+				}
+			}
+			return m_messages.empty();
+		}
+
+	};
 }
--- common/qcrules/gloss_check_test.cpp
+++ common/qcrules/gloss_check_test.cpp
@@ -5,18 +5,60 @@
 #include "StdAfx.h"
 #include "Array.h"
 
+#include "gloss_check.h"
 #include <boost/test/unit_test.hpp>
 
 #ifdef UNIT_TEST
 
-BOOST_AUTO_TEST_SUITE( GlossCheckTests )
+using namespace qc ;
+using namespace std ;
 
-	/** @test begin() method of class Array.
-	 */
+BOOST_AUTO_TEST_SUITE( GlossCheckRuleTests )
+
 	BOOST_AUTO_TEST_CASE( ensure_init )
 	{
-		BOOST_CHECK_EQUAL( 1, 1 ) ;
+		GlossCheckRule rule ;
+		vector<wstring> messages ;
+		rule.get_error_msgs(messages) ;
+		BOOST_CHECK_EQUAL( 0u, messages.size() ) ;
+	}
+	BOOST_AUTO_TEST_CASE( empty_source_and_target )
+	{
+		GlossCheckRule rule ;
+		wstring source = L"" ;
+		wstring target = L"" ;
+		BOOST_CHECK( rule.check(source, target) ) ;
+	}
 
+	BOOST_AUTO_TEST_CASE( gloss_match )
+	{
+		GlossCheckRule rule ;
+		rule.add_gloss_match(L"foo", L"bar") ;
+		wstring source = L"I have foo." ;
+		wstring target = L"I have bar." ;
+		BOOST_CHECK( rule.check(source, target) ) ;
+	}
+	BOOST_AUTO_TEST_CASE( non_gloss_match )
+	{
+		GlossCheckRule rule ;
+		rule.add_gloss_match(L"foo", L"bar") ;
+		wstring source = L"I have foo." ;
+		wstring target = L"I have foo." ;
+		BOOST_CHECK( ! rule.check(source, target) ) ;
+	}
+	BOOST_AUTO_TEST_CASE( non_gloss_match_messages )
+	{
+		GlossCheckRule rule ;
+		rule.add_gloss_match(L"foo", L"bar") ;
+		wstring source = L"I have foo." ;
+		wstring target = L"I have foo." ;
+		rule.check(source, target) ;
+		vector<wstring> messages ;
+		rule.get_error_msgs(messages) ;
+		BOOST_CHECK_EQUAL(1u, messages.size()) ;
+		wstring message = messages[0] ;
+		wstring expected = L"Gloss Check: Word 'foo' in source but 'bar' not in target" ;
+		BOOST_CHECK_EQUAL(message, expected) ;
 	}
 BOOST_AUTO_TEST_SUITE_END()
 
