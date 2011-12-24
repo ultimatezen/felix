222db6a3d5c89b520826b15fe5769b90eef4b980
ginstrom <ginstrom@localhost>
2011-12-24 15:40:48 -0800

    Got rid of vxtools

--- common/vxtool/Examples/CustomTooltips/StdAfx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	CustomTooltips.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-CComModule _Module; // Implementing ATL support
