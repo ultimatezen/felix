ead21bc56bbf92d421cc8b03710db71bdc7576d4
ginstrom <ginstrom@localhost>
2011-12-27 11:09:26 -0800

    Added unit tests; added files to Felix project

---
+++ Felix/GlossaryWindow_test.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "GlossaryWindow.h"
+
+#include <boost/test/unit_test.hpp>
+
+#ifdef UNIT_TEST
+
+#include "amop/include/MockObject.h"
+
+// test_properties_loaded_history
+BOOST_AUTO_TEST_SUITE( TestGlossaryWindow )
+
+	typedef CComObject< CGlossaryWindow > gw_obj ;
+	typedef CComPtr< gw_obj > gw_ptr ;
+
+	BOOST_AUTO_TEST_CASE( Instantiate )
+	{
+		gw_ptr gloss ;
+		HRESULT hr = gw_obj::CreateInstance( &gloss ) ;
+		BOOST_CHECK( SUCCEEDED( hr ) ) ;
+	}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
