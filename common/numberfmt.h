3703d81ead0a50f469800f6a77d72f6eabfc8a93
ginstrom <ginstrom@localhost>
2012-06-02 14:03:33 -0700

    Bug in numberfmt.h; bumped version to 1.7

--- common/numberfmt.h
+++ common/numberfmt.h
@@ -29,8 +29,8 @@ public:
 		ZeroMemory(out_buff, OUTBUFSIZE * sizeof(TCHAR)) ;
 
 		// get our separators
-		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SDECIMAL, &m_DecSep[0], SEPSIZE * sizeof( TCHAR ) );
-		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_STHOUSAND, &m_ThousandsSep[0], SEPSIZE * sizeof( TCHAR ));
+		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SDECIMAL, &m_DecSep[0], SEPSIZE );
+		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_STHOUSAND, &m_ThousandsSep[0], SEPSIZE );
 
 		m_nf.lpDecimalSep = m_DecSep;
 		m_nf.lpThousandSep = m_ThousandsSep;
@@ -42,13 +42,13 @@ public:
 		TCHAR buff[BUFF_SIZE];
 
 		/* But all the system defaults for the others */
-		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_ILZERO, &buff[0], BUFF_SIZE * sizeof(TCHAR) );
+		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_ILZERO, &buff[0], BUFF_SIZE );
 		m_nf.LeadingZero = _ttoi( buff );
 
-		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SGROUPING, &buff[0], BUFF_SIZE * sizeof(TCHAR) ) ;
+		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SGROUPING, &buff[0], BUFF_SIZE ) ;
 		m_nf.Grouping = _ttoi( buff );
 
-		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_INEGNUMBER, &buff[0], BUFF_SIZE * sizeof(TCHAR) ) ;
+		GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_INEGNUMBER, &buff[0], BUFF_SIZE ) ;
 		m_nf.NegativeOrder = _ttoi( buff );
 	}
 
@@ -78,7 +78,7 @@ public:
 			return CString() ;
 		}
 
-		GetNumberFormat( LOCALE_USER_DEFAULT, 0, in_buffer, &m_nf, out_buff, OUTBUFSIZE * sizeof(TCHAR) ) ;
+		GetNumberFormat( LOCALE_USER_DEFAULT, 0, in_buffer, &m_nf, out_buff, OUTBUFSIZE ) ;
 
 		return CString(out_buff) ;
 	}
@@ -89,7 +89,7 @@ public:
 	{
 		_ultot_s( static_cast< unsigned long >( num ), in_buffer, INBUFSIZE, 10 ) ;
 
-		GetNumberFormat( LOCALE_USER_DEFAULT, 0, in_buffer, &m_nf, out_buff, sizeof( out_buff ) ) ;
+		GetNumberFormat( LOCALE_USER_DEFAULT, 0, in_buffer, &m_nf, out_buff, OUTBUFSIZE ) ;
 
 		return CString(out_buff) ;
 	}
