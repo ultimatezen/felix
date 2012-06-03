98b93e06e38d74211ced419ee151bfa3751d09be
ginstrom <ginstrom@localhost>
2012-06-03 15:14:09 -0700

    Continue developing gloss_placement

--- Felix/gloss_placement.h
+++ Felix/gloss_placement.h
@@ -4,9 +4,77 @@
 
 namespace mem_engine
 {
+#ifdef min
+#undef min
+#endif
+
+	// Place glossary matches.
 	class gloss_placement
 	{
 	public:
+
+		// hole defs.
+		struct hole_t
+		{
+			size_t start ;
+			size_t len ;
+			hole_t() : start(0), len(0) {}
+			hole_t(size_t s, size_t l) : start(s), len(l) {}
+		};
+		struct hole_pair_t
+		{
+			hole_t lhs ;
+			hole_t rhs ;
+			hole_pair_t() {}
+			hole_pair_t(hole_t l, hole_t r) : lhs(l), rhs(r) {}
+		};
+
+		// Find a 'hole' between `lhs` and `rhs`.
+		// A hole is a mismatched segment between the two strings.
+		// Example: 
+		//		AAA BBB XXX CCC
+		//		AAA BBB YYY CCC
+		// Here, the hole is XXX vs YYY.
+		bool find_hole(const wstring &lhs, const wstring &rhs, hole_pair_t &holes) const
+		{
+			// If either string is empty, there is no hole.
+			if (lhs.empty() || rhs.empty())
+			{
+				return false ;
+			}
+
+			// Advance over the part at the beginning that matches.
+			size_t start = 0 ;
+			while(start < std::min(lhs.size(), rhs.size()) && lhs[start] == rhs[start])
+			{
+				++start ;
+			}
+
+			// Advance over the part at the end that matches.
+			size_t end1 = lhs.size() ;
+			size_t end2 = rhs.size() ;
+			while(end1 && end2 && lhs[end1-1] == rhs[end2-1])
+			{
+				--end1 ;
+				--end2 ;
+			}
+
+			// Define the hole.
+			holes.lhs.start = start ;
+			holes.rhs.start = start ;
+
+			holes.lhs.len = end1 - start ;
+			holes.rhs.len = end2 - start ;
+
+			return true ;
+	
+		}
+
+		// Translate a hole into a substring.
+		wstring str_hole(const wstring text, const hole_t h) const
+		{
+			return text.substr(h.start, h.len) ;
+		}
 
 	};
 }
---
+++ Felix/gloss_placement_test.cpp
@@ -0,0 +1,128 @@
+#include "stdafx.h"
+#include "gloss_placement.h"
+
+#ifdef UNIT_TEST
+
+#include <boost/test/unit_test.hpp>
+BOOST_AUTO_TEST_SUITE( gloss_placement_tests )
+
+	using namespace mem_engine ;
+
+	// find_hole
+	BOOST_AUTO_TEST_CASE(empty_lhs)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"" ;
+		wstring rhs = L"abc" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(! placement.find_hole(lhs, rhs, holes)) ;
+	}
+	BOOST_AUTO_TEST_CASE(empty_rhs)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"abc" ;
+		wstring rhs = L"" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(! placement.find_hole(lhs, rhs, holes)) ;
+	}
+
+	// find_hole - start
+	BOOST_AUTO_TEST_CASE(start_0)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"xxx" ;
+		wstring rhs = L"yyy" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.start, 0u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.start, 0u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_1)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"axxx" ;
+		wstring rhs = L"ayyy" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.start, 1u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.start, 1u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_2)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"abxxx" ;
+		wstring rhs = L"abyyy" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.start, 2u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.start, 2u) ;
+	}
+	// find_hole - end
+	BOOST_AUTO_TEST_CASE(start_0_len_3)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"xxx" ;
+		wstring rhs = L"yyy" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.len, 3u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.len, 3u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_3_len_0)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"aaa" ;
+		wstring rhs = L"aaa" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.start, 3u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.start, 3u) ;
+		BOOST_CHECK_EQUAL(holes.lhs.len, 0u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.len, 0u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_0_len_3_end_match)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"xxxaa" ;
+		wstring rhs = L"yyyaa" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.len, 3u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.len, 3u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_1_len_1)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"axbb" ;
+		wstring rhs = L"aybb" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.len, 1u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.len, 1u) ;
+	}
+	BOOST_AUTO_TEST_CASE(start_2_len_2)
+	{
+		gloss_placement placement ;
+		wstring lhs = L"abxxcd" ;
+		wstring rhs = L"abyycd" ;
+		gloss_placement::hole_pair_t holes ;
+		BOOST_CHECK(placement.find_hole(lhs, rhs, holes)) ;
+		BOOST_CHECK_EQUAL(holes.lhs.start, 2u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.start, 2u) ;
+		BOOST_CHECK_EQUAL(holes.lhs.len, 2u) ;
+		BOOST_CHECK_EQUAL(holes.rhs.len, 2u) ;
+	}
+	// str_hole
+	BOOST_AUTO_TEST_CASE(str_hole)
+	{
+		gloss_placement placement ;
+		wstring expected = L"12" ;
+		wstring text = L"xxx12xxx" ;
+		gloss_placement::hole_t h(3, 2) ;
+		BOOST_CHECK_EQUAL(placement.str_hole(text, h), expected) ;
+	}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
