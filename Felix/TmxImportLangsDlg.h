eae1d98b5082c2671d79fd4de4141387d075e389
ginstrom <ginstrom@localhost>
2013-06-10 01:00:18 -0700

    Extracted common TMX dialog includes/typedefs

--- Felix/TmxImportLangsDlg.h
+++ Felix/TmxImportLangsDlg.h
@@ -5,13 +5,7 @@
 
 #pragma once
 
-#include "Exceptions.h"		// exception support
-#include "WindowExceptionHandler.h"
-
-#include "LanguageHolder.h"
-
-typedef std::vector< tstring >		string_list ;
-typedef std::map< tstring, tstring >	string_map ;
+#include "tmx_dialogs.h"
 
 // CTMXImportLangsDlg
 class CTMXImportLangsDlg : 
---
+++ Felix/tmx_dialogs.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Exceptions.h"		// exception support
+#include "WindowExceptionHandler.h"
+
+#include "LanguageHolder.h"
+
+typedef std::vector< tstring >		string_list ;
+typedef std::map< tstring, tstring >	string_map ;
