6e491d40404d46e185a66fed7f0ac051f3e80e01
ginstrom <ginstrom@localhost>
2013-07-10 10:44:48 -0700

    Added all MemoryWindow functionality; moved implemenations to source file

--- Felix/FelixModelInterface.h
+++ Felix/FelixModelInterface.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "memory_model.h"
+#include "file.h"
 
 class FelixModelInterface
 {
@@ -9,47 +10,42 @@ public:
 	typedef mem_engine::memory_pointer memory_type ;
 	typedef std::list< memory_type > memory_list ;
 
-	virtual ~FelixModelInterface(){}
+	virtual ~FelixModelInterface();
 
-	size_t size()
-	{
-		return this->get_memories()->size() ;
-	}
-	memory_type memory_at(size_t i)
-	{
-		return this->get_memories()->get_memory_at(i) ;
-	}
-	mem_engine::memory_iterator begin()
-	{
-		return this->get_memories()->begin() ;
-	}
-	mem_engine::memory_iterator end()
-	{
-		return this->get_memories()->end() ;
-	}
-	void remove_memory(memory_type mem)
-	{
-		this->get_memories()->remove_memory_by_id(mem->get_id()) ;
-	}
+	// querying
+	size_t size();
+	bool empty();
+	bool has_name_clash(CString location);
+	void record_loaded_memories(std::vector<wstring> &local, std::vector<wstring> &remote);
 
-	memory_type add_memory()
-	{
-		return this->get_memories()->add_memory() ;
-	}
+	// getting
+	mem_engine::memory_iterator begin();
+	memory_type memory_at(size_t i);
+	virtual mem_engine::memory_pointer get_first_memory();
+
+	// removing
+	void remove_memory(memory_type mem);
+	bool remove_memory_by_id( const size_t memid );
+	virtual void clear();
+
+	void remove_record(mem_engine::record_pointer record);
+
+	// adding
+	void insert_memory(memory_type mem);
+	memory_type add_memory();
+	memory_type create_memory();
+
+	// searching
 	bool perform_search( mem_engine::search_match_container &matches, 
-		const mem_engine::search_query_params &params )
-	{
-		return this->get_memories()->perform_search(matches, params) ;
-	}
-	virtual mem_engine::memory_pointer get_first_memory()
-	{
-		return this->get_memories()->get_first_memory() ;
-	}
-	virtual void clear()
-	{
-		this->get_memories()->clear() ;
-	}
+		const mem_engine::search_query_params &params );
+	bool find_matches ( mem_engine::trans_match_container &matches, 
+		const mem_engine::search_query_params &params );
+	bool find_trans_matches ( mem_engine::trans_match_container &matches, 
+		const mem_engine::search_query_params &params );
+
+	//////////////////////////////////////////////////////////////////////////
 	// pure virtual functions
+	//////////////////////////////////////////////////////////////////////////
 	virtual model_ptr get_memories() = 0 ;
 	virtual void set_reverse_lookup(bool setting) = 0 ;
 	virtual bool is_reverse_lookup() = 0 ;
@@ -59,69 +55,6 @@ public:
 	virtual model_ptr create_memory_model(bool is_memory) = 0 ;
 };
 
-class FelixModel : public FelixModelInterface
-{
-public:
-	bool m_is_reverse_lookup ;
-
-	model_ptr m_memories ;
-
-	app_props::props_ptr m_props; 
-
-
-	FelixModel(app_props::props_ptr props,
-		bool is_memory=true) :
-		m_is_reverse_lookup(false),
-		m_props(props)
-	{
-		m_memories = create_memory_model(is_memory) ;
-	}
-
-	virtual ~FelixModel(void)
-	{
-	}	
-
-	model_ptr create_memory_model(bool is_memory=false)
-	{
-		if (is_memory)
-		{
-			return model_ptr(new mem_engine::memory_model_mem(m_props)) ;
-		}
-		else
-		{
-			return model_ptr(new mem_engine::memory_model_gloss(m_props)) ;
-		}
-	}
-
-	size_t get_first_mem_id()
-	{
-		memory_type mem = m_memories->get_first_memory() ;
-		return mem->get_id() ;
-	}
-
-	model_ptr get_memories()
-	{
-		return m_memories ;
-	}
-	void set_reverse_lookup(bool setting)
-	{
-		m_is_reverse_lookup = setting ;
-	}
-	bool is_reverse_lookup()
-	{
-		return m_is_reverse_lookup ;
-	}
-
-	mem_engine::memory_pointer get_memory_by_id(size_t id)
-	{
-		return m_memories->get_memory_by_id(id) ;
-	}
-	void get_memories_needing_saving( memory_list &memories )
-	{
-		m_memories->get_memories_needing_saving(memories) ;
-	}
-
-};
 
 typedef boost::shared_ptr<FelixModelInterface> model_iface_ptr ;
 
