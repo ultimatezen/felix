e540e33afa51b0f8a38026d5d062d58de0483292
ginstrom <ginstrom@localhost>
2013-10-31 22:18:58 -0700

    Created string_logger class to use logging in unit tests

---
+++ Felix/string_logger.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "logging.h"
+
+#ifdef UNIT_TEST
+
+#include <sstream>
+
+class string_logger : public logger
+{
+
+public:
+	std::stringstream out ;
+
+	string_logger(){}
+	~string_logger(){}
+
+	void write_entry(const string level, const string msg)
+	{
+		out << level << "\t" << msg << "\n" ;
+	}
+	void send_report(const CString language, const CString filename)
+	{
+		out << "send_report: " << static_cast<LPCSTR>(CStringA(language)) << " " << static_cast<LPCSTR>(CStringA(language)) << "\n" ;
+	}
+};
+
+
+struct StringLoggerTester 
+{
+	StringLoggerTester() : out(new string_logger) { logging::set_logger(logger_ptr(out)); }
+	string get_log() { return out->out.str() ;}
+	string_logger *out;
+};
+
+#endif // UNIT_TEST
