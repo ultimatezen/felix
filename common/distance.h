dbd2d614a142aca1ff7b54f864e75968c70917f4
ginstrom <ginstrom@localhost>
2014-10-26 22:24:53 -0700

    added comments

--- common/distance.h
+++ common/distance.h
@@ -1,10 +1,17 @@
+/*
+	Calculates edit distance, and similarity score (0 to 1) based on 
+	edit distance.
+*/
+
 #pragma once
 
+// minimum of three values
 size_t min3(size_t a, size_t b, size_t c) ;
 // Gets the score, given the highest length and diff (total cost)
 // side effect: Logs warning on divide by 0 error.
 double compute_score( const size_t high_len, size_t total_cost ) ;
 
+// Class to calculate edit distance and similarity score
 class Distance
 {
 	size_t *row1 ;
@@ -25,6 +32,4 @@ public:
 	double subdist_score(const std::wstring &needle, const std::wstring &haystack);
 
 	void ensure_size(size_t min_row_size) ;
-	// minimum of three values
-
 };
